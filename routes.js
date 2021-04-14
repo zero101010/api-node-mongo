7f798f0c236775f1417a5aa93ce87c6d306148dc
Igor Araujo de Sousa <igotaraujo97@gmail.com>
2021-04-14 12:09:11 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -9,7 +9,7 @@ routes.get("/clients", DocumentsController.index);
 routes.post("/login", Authenticate.login);
 
 routes.get("/", (req,res)=>{
-    return res.json({"Version":"5","Status":"Up"}); 
+    return res.json({"Version":"6","Status":"Up"}); 
 });
 
 module.exports = routes;
